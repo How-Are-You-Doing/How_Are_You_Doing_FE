46a3344001e30593651ee433ed49d9e0c4260636
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-11-03 16:16:59 -0600

    bug fixgit add .git add .

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
 
   def create
-    user = User.find_or_create_by(user_params[:email])
+    user = User.find_or_create_by(email: user_params[:email])
     user.update(user_params)
 
     
