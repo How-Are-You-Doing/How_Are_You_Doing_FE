d4f16611fbf1b8efda11ea09e3b3fc6f82871b4c
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-11-07 18:33:22 -0700

    sad path test

--- spec/features/posts/index_spec.rb
+++ spec/features/posts/index_spec.rb
@@ -26,8 +26,19 @@ RSpec.describe 'history index page', type: :feature do
             end
           end
         end
+
+        it "If i am a user with no posts I see 'You have no posts yet' on my history page" do
+          VCR.use_cassette('lonely_user_info') do
+            user_2_lonely = User.create!(name: "Lonely", email: "noposts@lonely.alone", google_id: 8675310, token: 'fake_token')
+            user_posts = DatabaseFacade.user_post_history(user_2_lonely.google_id)
+  
+            visit '/history'
+  
+            expect(page).to have_content("How You've Been Doing Lately")
+            expect(page).to have_content("You don't have any posts.")
+          end
+        end
       end
     end
   end
 end
-
